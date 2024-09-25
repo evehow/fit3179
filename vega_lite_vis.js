083c9aad9fc2144739fb2f7e5424aec91f14a7c0
evehow <127315802+evehow@users.noreply.github.com>
2024-09-25 19:26:21 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
 var vg_1 = "meteorite_landings_map.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function (result) {
+vegaEmbed("#choropleth_map", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
