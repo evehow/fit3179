325083f5b510ecb742362dfa6df7d642fff88640
evehow <127315802+evehow@users.noreply.github.com>
2024-10-07 17:11:35 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,7 @@ var vg_1 = "meteorite_landings_map.vg.json";
 vegaEmbed("#proportional_symbol_map", vg_1).then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_2 = "size_year_meteorite_landings.vg.json";
+vegaEmbed("#scatterplot", vg_2).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
